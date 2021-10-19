82437308d3f653489d9c2deedbe9d4b218203748
Andrew Gurin <gurinos68@gmail.com>
2021-10-19 15:40:14 +0700

    feat: trying to make any comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import { NameInput } from "./Components//atoms/Button/index";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
   return (
     <div className="App">
-      <heade className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hi_hi_hi
-        </a>
-      </heade>
+      <NameInput value={inputValue} onChange={setInputValue}></NameInput>
     </div>
   );
 }
--- src/Components/atoms/Button/index.tsx
+++ src/Components/atoms/Button/index.tsx
@@ -1,2 +1,11 @@
-import { Component } from "react";
-import "./main.sass";
+import React from "react";
+import "./main.css";
+interface INameInput {
+  label?: string;
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+}
+export const NameInput: React.FC<INameInput> = () => {
+  return <div className="nameimput">Hello TS</div>;
+};
