ae235cbb400038db19dd6fae8db70975acb7387b
Andrew Gurin <gurinos68@gmail.com>
2021-10-21 02:16:54 +0700

    feat: add frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Frame } from './Components/moleculs/Frame/index'
-import { Text } from "./Components/atoms/Text/index"
+import { Frame } from './Components/moleculs/Frame/index';
+import { Text } from './Components/atoms/Text/index';
 function App() {
   const [inputValue, setInputValue] = useState('');
   return (
     <div className="App">
-      <Frame>
-
-      </Frame>
+      <Frame></Frame>
     </div>
   );
 }
